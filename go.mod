9807af51658933411879e6167b0a362a9ecb7aac
Chad Serrant <therealchadius@gmail.com>
2022-01-29 10:54:37 -0500

    Adding directories.

--- go.mod
+++ go.mod
@@ -1,13 +1,23 @@
 module github.com/chadius/terosgameserver
 
-go 1.15
-
-replace github.com/chadius/terosgamerules v0.0.0-20220114204417-1dca36259870 => ../terosGameRules
+go 1.17
 
 require (
 	github.com/chadius/terosgamerules v0.0.0-20220114204417-1dca36259870
-	github.com/maxbrunsfeld/counterfeiter/v6 v6.4.1 // indirect
+	github.com/maxbrunsfeld/counterfeiter/v6 v6.4.1
+	github.com/stretchr/testify v1.7.0
+	github.com/twitchtv/twirp v8.1.1+incompatible
+	google.golang.org/protobuf v1.23.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/twitchtv/twirp v8.1.1+incompatible // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/mod v0.3.0 // indirect
+	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- terosgameserver_test.go
+++ terosgameserver_test.go
@@ -1,39 +1,97 @@
 package terosGameServer_test
 
 import (
-	"github.com/chadius/terosGameServer/terosgamerulesfakes"
-	. "gopkg.in/check.v1"
+	"bytes"
+	"github.com/Chadius/terosGameServer/rpc/github.com/chadius/teros_game_server"
+	"github.com/chadius/terosGameServer/rulesstrategyfakes"
+	"github.com/chadius/terosgamerules"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+func TestServerUsesPackageSuite(t *testing.T) {
+	suite.Run(t, new(ServerUsesPackageSuite))
+}
 
-type UsePackageTestSuite struct{}
+type ServerUsesPackageSuite struct {
+	suite.Suite
+	request                         *http.Request
+	responseRecorder                *httptest.ResponseRecorder
+	server         teros_game_server.TwirpServer
+	fakeGameRules *terosgamerules.RulesStrategy
+	scriptData                      []byte
+	squaddieData       []byte
+	powerData         []byte
+	gameRulesResponse []byte
+}
 
-func (suite *UsePackageTestSuite) TestWhenFilesAreSupplied_ThenCallPackage(checker *C) {
-	// Setup
-	squaddieByteStream := []byte(`squaddies go here`)
-	powerByteStream := []byte(`powers go here`)
-	scriptByteStream := []byte(`script goes here`)
-	expectedResponse := []byte(`rules responded`)
+func (suite *ServerUsesPackageSuite) SetupTest() {
+	suite.scriptData = []byte(`images go here`)
+	suite.squaddieData = []byte(`formula goes here`)
+	suite.powerData = []byte(`outputSettings go here`)
+	requestBody := suite.getDataStream()
+	suite.request = suite.generateProtobufRequest(requestBody)
+	suite.responseRecorder = httptest.NewRecorder()
 
-	// TODO turn the package terosGameRules into an Interface
-	fakeGameRules := terosgamerulesfakes.FakeRulesStrategy{}
-	fakeGameRules.ProcessAttackScriptStub = func(scriptFileHandle, squaddieFileHandle, powerFileHandle io.Reader, output io.Writer) error {
+	suite.gameRulesResponse = []byte(`rules responded`)
+	suite.fakeGameRules = suite.fakeGameRulesWithResponse(suite.gameRulesResponse)
+	suite.server = suite.getServer()
+}
+
+func (suite *ServerUsesPackageSuite) fakeGameRulesWithResponse(expectedResponse []byte) *creatingsymmetryfakes.FakeTransformerStrategy {
+	fakeTransformerStrategy := rulesstrategyfakes.FakeRulesStrategy{}
+	fakeTransformerStrategy.ReplayBattleScriptStub = func(scriptFileHandle, squaddieFileHandle, powerFileHandle io.Reader, output io.Writer) error {
 		output.Write(expectedResponse)
 		return nil
 	}
+	return &fakeTransformerStrategy
+}
 
+func (suite *ServerUsesPackageSuite) getServer() image_transform_server.TwirpServer {
+	server := transformserver.NewServer(suite.fakeGameRules)
+	twirpServer := image_transform_server.NewImageTransformerServer(server)
+	return twirpServer
+}
+
+func (suite *ServerUsesPackageSuite) generateProtobufRequest(requestBody *bytes.Buffer) *http.Request {
+	testRequest, newRequestErr := http.NewRequest(
+		http.MethodPost,
+		"/twirp/chadius.imageTransformServer.ImageTransformer/Transform",
+		requestBody,
+	)
+	require := require.New(suite.T())
+	require.Nil(newRequestErr)
+	testRequest.Header.Set("Content-Type", "application/protobuf")
+	return testRequest
+}
+
+func (suite *ServerUsesPackageSuite) getDataStream() *bytes.Buffer {
+	dataStream := &image_transform_server.DataStreams{
+		InputImage:     suite.scriptData,
+		FormulaData:    suite.squaddieData,
+		OutputSettings: suite.outputSettingsData,
+	}
+
+	protobuf, protobufErr := proto.Marshal(dataStream)
+	requestBody := bytes.NewBuffer(protobuf)
+
+	require := require.New(suite.T())
+	require.Nil(protobufErr)
+
+	return requestBody
+}
+
+func (suite *ServerUsesPackageSuite) TestWhenClientMakesRequest_ResponseIsValid() {
 	// Act
-	actualResponse := terosserver.ProcessAttackScript(scriptByteStream, squaddieByteStream, powerByteStream)
+	suite.server.ServeHTTP(suite.responseRecorder, suite.request)
 
 	// Assert
-	actualScriptFileHandle, actualSquaddieFileHandle, actualPowerFileHandle, _ := fakeGameRules.ProcessAttackScriptArgsForCall(0)
-	checker.Assert(actualScriptFileHandle, Equals, scriptByteStream)
-	checker.Assert(actualSquaddieFileHandle, Equals, squaddieByteStream)
-	checker.Assert(actualPowerFileHandle, Equals, powerByteStream)
+	response := suite.responseRecorder.Result()
 
-	checker.Assert(200, Equals, actualResponse.ResponseCode)
-	checker.Assert(expectedResponse, Equals, actualResponse.Data)
+	require := require.New(suite.T())
+	require.Equal(200, response.StatusCode, "Status code is wrong")
 }
